03a1528f7c43ae5663ef569df34aff6eadd7b716
Wang, Reese <rwang706@gatech.edu>
2024-02-20 22:50:25 -0500

    enter login page from welcome page

--- app/src/main/java/com/example/recipeapp/WelcomeActivity.java
+++ app/src/main/java/com/example/recipeapp/WelcomeActivity.java
@@ -2,7 +2,9 @@ package com.example.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button gotoLoginScreenButton = findViewById(R.id.loginButton);
+        gotoLoginScreenButton.setOnClickListener(view ->{
+            Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
