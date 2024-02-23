df92ff4161d274155b576c70eb14f7e124d75bbb
Wang, Reese <rwang706@gatech.edu>
2024-02-23 12:33:03 -0500

    moved all navbar logic to welcomeactivity

--- app/src/main/java/com/example/recipeapp/views/MainActivity.java
+++ app/src/main/java/com/example/recipeapp/views/MainActivity.java
@@ -19,44 +19,13 @@ import com.example.recipeapp.model.ShoppingList;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView
-        .OnNavigationItemSelectedListener{
-    HomeFragment homeFragment = new HomeFragment();
-    ShoppingListFragment shoppingListFragment = new ShoppingListFragment();
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView navbar = findViewById(R.id.bottomNavigationView);
-        navbar.setOnNavigationItemSelectedListener(this);
-        navbar.setSelectedItemId(R.id.homeNavbarIcon);
-
-    }
-
-    @Override
-    public boolean
-    onNavigationItemSelected(MenuItem item)
-    {
-        int viewId = item.getItemId();
-        if (viewId == R.id.homeNavbarIcon){
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.flFragment, homeFragment)
-                    .commit();
-            return true;
-
-        } else if (viewId == R.id.shopping_listNavbarIcon){
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.flFragment, shoppingListFragment)
-                    .commit();
-            return true;
-
-        } else {
-            return false;
-        }
     }
 
 
