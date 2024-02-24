e7f2d219a0f755fb1adb8dba7f46a934ac633118
hssathu4596 <hssathu4596@gmail.com>
2024-02-24 16:38:20 -0500

    Added a comment for the Home Fragment

--- app/src/main/java/com/example/recipeapp/HomeFragment.java
+++ app/src/main/java/com/example/recipeapp/HomeFragment.java
@@ -12,6 +12,18 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
+    /**
+     * Goes and creates a view for the Home part of the nav bar(fragment)
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return a View
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
