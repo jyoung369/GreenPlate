9892a35990fa84ee2ad768cae277e5078b27566a
Wang, Reese <rwang706@gatech.edu>
2024-02-23 22:11:11 -0500

    successful login goes to welcome/home screen

--- app/src/main/java/com/example/recipeapp/views/LoginActivity.java
+++ app/src/main/java/com/example/recipeapp/views/LoginActivity.java
@@ -51,7 +51,7 @@ public class LoginActivity extends AppCompatActivity{
                     @Override
                     public void onSuccess(AuthResult authResult) {
                         Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        startActivity(new Intent(LoginActivity.this, WelcomeActivity.class));
                         finish();
                     }
                 }).addOnFailureListener(new OnFailureListener() {
