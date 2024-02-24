c222253c8dc4188b8e23eb4fc33b0467b379e14f
hssathu4596 <hssathu4596@gmail.com>
2024-02-24 15:36:10 -0500

    Added a comment for the splash page part

--- app/src/main/java/com/example/recipeapp/views/MainActivity.java
+++ app/src/main/java/com/example/recipeapp/views/MainActivity.java
@@ -28,6 +28,14 @@ public class MainActivity extends AppCompatActivity{
 //        setContentView(R.layout.splash_screen);
 //    }
 
+    /**
+     * Creates the instance of the splash screen to be viewed before the app actually launches
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
