ce8f676b36023c148e4c767b3c2221657c4e2d09
simonaa26 <isim.linux@gmail.com>
2024-03-08 02:04:19 -0500

    added inputmeal screen to navbar

--- app/src/main/java/com/example/recipeapp/InputMealFragment.java
+++ app/src/main/java/com/example/recipeapp/InputMealFragment.java
@@ -6,42 +6,53 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class InputMealFragment extends Fragment {
 
-    public InputMealFragment() {
-        // Required empty public constructor
+//    public InputMealFragment() {
+//        // Required empty public constructor
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        View view = inflater.inflate(R.layout.fragment_input_meal, container, false);
+//
+//        // Find references to the buttons in the inflated layout
+//        Button button1 = view.findViewById(R.id.button1);
+//        Button button2 = view.findViewById(R.id.button2);
+//
+//        // Set click listener for Button 1
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Handle button1 click
+//                // Perform an action, e.g., start another activity or execute some logic
+//            }
+//        });
+//
+//        // Set click listener for Button 2
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Handle button2 click
+//                // Perform an action, e.g., start another activity or execute some logic
+//            }
+//        });
+//
+//        return view;
+//    }
+    public InputMealFragment newInstance() {
+        return new InputMealFragment();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_input_meal, container, false);
-
-        // Find references to the buttons in the inflated layout
-        Button button1 = view.findViewById(R.id.button1);
-        Button button2 = view.findViewById(R.id.button2);
-
-        // Set click listener for Button 1
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle button1 click
-                // Perform an action, e.g., start another activity or execute some logic
-            }
-        });
-
-        // Set click listener for Button 2
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle button2 click
-                // Perform an action, e.g., start another activity or execute some logic
-            }
-        });
-
-        return view;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_input_meal, container, false);
     }
 }
