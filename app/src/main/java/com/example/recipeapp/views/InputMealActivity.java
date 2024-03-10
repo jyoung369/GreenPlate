a7c03036cafc2fbea10a55aecc0242c5d610c2a3
simonaa26 <isim.linux@gmail.com>
2024-03-10 09:14:06 -0400

    meals input into database correctly

--- app/src/main/java/com/example/recipeapp/views/InputMealActivity.java
+++ app/src/main/java/com/example/recipeapp/views/InputMealActivity.java
@@ -1,6 +1,7 @@
 package com.example.recipeapp.views;
 
 import static android.content.ContentValues.TAG;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,7 +21,9 @@ import android.widget.Toast;
 import com.example.recipeapp.R;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class InputMealActivity extends AppCompatActivity {
@@ -36,6 +39,21 @@ public class InputMealActivity extends AppCompatActivity {
         Button input = findViewById(R.id.inputButton);
         input.setOnClickListener(view -> inputMeal());
     }
+
+    class Meal {
+        private String name;
+        private String calories;
+        public Meal (String name, String calories) {
+            this.name = name;
+            this.calories = calories;
+        }
+        public String getMealName() {
+            return name;
+        }
+        public String getCalories() {
+            return calories;
+        }
+    }
     private void inputMeal() {
             String nameOfMeal = mealName.getText().toString();
             String cals = calories.getText().toString();
@@ -44,16 +62,14 @@ public class InputMealActivity extends AppCompatActivity {
             } else if (cals.isEmpty()) {
                 calories.setError("Please enter the amount of calories in your meal!");
             } else {
-                Map<String, Object> mealData = new HashMap<>();
-                mealData.put("name", nameOfMeal);
-                mealData.put("calorie count", cals);
-                mealData.put("userid", user.getUid());
+                Meal myMeal = new Meal(nameOfMeal, cals);
 
-                FirebaseDatabase database = FirebaseDatabase.getInstance("https://recipeapp-1fba1-default-rtdb.firebaseio.com/");
-                DatabaseReference mealsref = database.getReference().child("meals");
+                FirebaseDatabase database = FirebaseDatabase
+                        .getInstance("https://recipeapp-1fba1-default-rtdb.firebaseio.com/");
+                DatabaseReference mealsref = database.getReference().child("meals/"
+                        + user.getUid());
 
-                mealsref.child(user.getUid())
-                        .setValue(mealData)
+                mealsref.push().setValue(myMeal)
                         .addOnSuccessListener(success -> {
                             Toast.makeText(InputMealActivity.this,
                                     "Meal inputted successfully!", Toast.LENGTH_SHORT).show();
