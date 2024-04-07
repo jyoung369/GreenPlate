aa1f0aaadc83ac883927c055603c6af3636211cd
Wang, Reese <rwang706@gatech.edu>
2024-04-06 23:52:31 -0400

    working shoppinglist setup

--- app/src/main/java/com/example/recipeapp/ShoppingListFragment.java
+++ app/src/main/java/com/example/recipeapp/ShoppingListFragment.java
@@ -2,11 +2,17 @@ package com.example.recipeapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.recipeapp.model.Ingredient;
+import com.example.recipeapp.viewmodels.ShoppingListViewModel;
 
 public class ShoppingListFragment extends Fragment {
 
@@ -19,5 +25,14 @@ public class ShoppingListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_shopping_list, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        // testing purposes: delete if you're implementing actual logic
+        ShoppingListViewModel shoppingListViewModel = new ViewModelProvider(this).get(ShoppingListViewModel.class);
+        shoppingListViewModel.addItem(getContext(), "abc", 6, 1200, "10-04-2024");
+        shoppingListViewModel.getShoppingList().observe(getViewLifecycleOwner(), list -> {
+            System.out.println("final shopping list: " + shoppingListViewModel.getShoppingList().getValue());
+        });
     }
 }
--- app/src/main/java/com/example/recipeapp/viewmodels/ShoppingListViewModel.java
+++ app/src/main/java/com/example/recipeapp/viewmodels/ShoppingListViewModel.java
@@ -1,4 +1,155 @@
 package com.example.recipeapp.viewmodels;
 
-public class ShoppingListViewModel {
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.recipeapp.model.Ingredient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ShoppingListViewModel extends ViewModel {
+    private final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    private MutableLiveData<Boolean> isSaveSuccessful = new MutableLiveData<>();
+
+    private List<Ingredient> allIngredients = new ArrayList<>();
+
+    private MutableLiveData<List<Ingredient>> shoppingList = new MutableLiveData<>();
+    public LiveData<List<Ingredient>> getShoppingList(){
+        return shoppingList;
+    }
+
+    public ShoppingListViewModel() {
+        // Initialize the shoppingList if needed
+    }
+
+    public void addItem(Context context, String name, Integer quantity, Integer caloriesPerServing, String expirationDate){
+        FirebaseDatabase shoppinglistDB = FirebaseDatabase
+                .getInstance("https://recipeapp-1fba1-default-rtdb.firebaseio.com/");
+        DatabaseReference shoppinglistRef = shoppinglistDB.getReference().child("shoppinglist/"
+                + user.getUid());
+        shoppinglistRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                boolean existsInList = false;
+                boolean shouldRemove = false;
+                String duplicateIngredientId = "";
+                Ingredient ingredient = new Ingredient(name, quantity, caloriesPerServing, expirationDate);
+                Ingredient duplicateIngredient = ingredient;
+                allIngredients = new ArrayList<>();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    String ingredientId = dataSnapshot.getKey();
+                    Ingredient currIngredient = dataSnapshot.getValue(Ingredient.class);
+                    allIngredients.add(currIngredient);
+
+                    if (currIngredient.getName().equals(name) && quantity > 0) {
+                        // modify
+                        existsInList = true;
+                        shouldRemove = false;
+                        duplicateIngredientId = ingredientId;
+                        duplicateIngredient = currIngredient;
+
+                    } else if (currIngredient.getName().equals(name) && quantity <= 0) {
+                        // remove
+                        existsInList = true;
+                        shouldRemove = true;
+                        duplicateIngredientId = ingredientId;
+                        duplicateIngredient = currIngredient;
+                    }
+                }
+                shoppingList.setValue(allIngredients);
+
+                if (existsInList && !shouldRemove) {
+                    shoppinglistRef.child(duplicateIngredientId).setValue(ingredient)
+                            .addOnSuccessListener(
+                                    aVoid -> isSaveSuccessful.postValue(true))
+                            .addOnFailureListener(
+                                    e -> isSaveSuccessful.postValue(false));
+                    List<Ingredient> currList = shoppingList.getValue();
+                    System.out.println("modify: previous ingredient: "+ duplicateIngredient);
+                    System.out.println("modify: new ingredient" + ingredient);
+                    currList.remove(duplicateIngredient); // remove previous
+                    currList.add(ingredient); // add new
+                    shoppingList.setValue(currList);
+                }
+                if (existsInList && shouldRemove) {
+                    shoppinglistRef.child(duplicateIngredientId).removeValue()
+                            .addOnSuccessListener(
+                                    aVoid -> isSaveSuccessful.postValue(true))
+                            .addOnFailureListener(
+                                    e -> isSaveSuccessful.postValue(false));
+                    List<Ingredient> currList = shoppingList.getValue();
+                    currList.remove(duplicateIngredient);
+                    System.out.println("modify: previous ingredient: "+ duplicateIngredient);
+                    shoppingList.setValue(currList);
+                }
+                if (!existsInList && quantity > 0) {
+                    // add to database
+                    shoppinglistRef.push().setValue(ingredient)
+                            .addOnSuccessListener(success -> {
+                                Toast.makeText(context,
+                                        "Ingredient inputted successfully!", Toast.LENGTH_SHORT).show();
+                            })
+                            .addOnFailureListener(failure -> {
+                                Toast.makeText(context,
+                                        "Could not input ingredient", Toast.LENGTH_SHORT).show();
+                            });
+                } else if (!existsInList && quantity <= 0) {
+                    Toast.makeText(context, "Cannot add negative or zero quantity of ingredient to list", Toast.LENGTH_SHORT).show();
+                }
+            }
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("FirebaseError", "Error reading data from Firebase: " + error.getMessage());
+            }
+        });
+    }
+
+    public void readShoppingList(){
+        FirebaseDatabase shoppinglistDB = FirebaseDatabase
+                .getInstance("https://recipeapp-1fba1-default-rtdb.firebaseio.com/");
+        DatabaseReference shoppinglistRef = shoppinglistDB.getReference().child("shoppinglist/"
+                + user.getUid());
+        shoppinglistRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    String ingredientName = dataSnapshot.child("name").getValue(String.class);
+                    Integer ingredientQuantity = dataSnapshot.child("quantity")
+                            .getValue(Integer.class);
+                    Integer calories = dataSnapshot.child("caloriesPerServing")
+                            .getValue(Integer.class);
+                    String expirationDate = dataSnapshot.child("expirationDate")
+                            .getValue(String.class);
+                    if (ingredientQuantity > 0) {
+                        // ingredientList.add(ingredientName);
+                        allIngredients.add(new Ingredient(ingredientName,
+                                ingredientQuantity, calories, expirationDate));
+                    }
+                    Log.d("FirebaseData",
+                            "Ingredient Name: " + ingredientName + ", Quantity: "
+                                    + ingredientQuantity + ", Calories: "
+                                    + calories + ", expirationDate: " + expirationDate);
+                }
+                shoppingList.setValue(allIngredients);
+            }
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("FirebaseError", "Error reading data from Firebase: " + error.getMessage());
+            }
+        });
+    }
 }
